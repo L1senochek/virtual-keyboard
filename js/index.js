5bdb232dbb0d399bed10be3a7b75a3c5d51f92ee
L1senochek <lisenochek96@gmail.com>
2023-04-26 21:43:33 +0300

    feat: add keyboard visual layout

--- js/index.js
+++ js/index.js
@@ -1,17 +1,23 @@
-const keys = [192, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 189, 187, 8,
-  81, 87, 69, 82, 84, 89, 85, 73, 79, 80, 219, 221, 220,
+const keysCode = [192, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48, 189, 187, 8,
+  9, 81, 87, 69, 82, 84, 89, 85, 73, 79, 80, 219, 221, 220,
   20, 65, 83, 68, 70, 71, 72, 74, 75, 76, 186, 222, 13,
   16, 90, 88, 67, 86, 66, 78, 77, 188, 190, 191, 38, 16,
   17, 91, 18, 32, 18, 37, 40, 39, 17];
-
+const keys = ['~ `', '! 1', '@ 2', '# 3', '$ 4', '% 5', '^ 6', '& 7', '* 8', '( 9', ') 0', '_ -', '+ =', 'backspace',
+  'tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '{ [', '} ]', '| \\',
+  'capslock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ': ;', `" '`, 'enter',
+  'shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', '< ,', '> .', '? /', '🠕', 'shift',
+  'ctrl', 'win', 'alt', 'space', 'alt', '🠔', '🠗', '🠖', 'ctrl'];
 const bodyTag = document.body;
 
 document.onkeydown = function (event) {
   console.log(event);
+  console.log(event.code);
 };
+console.log(keys.length);
+console.log(keysCode.length);
 
 bodyTag.classList.add('body');
-console.log(document.body);
 const wrapper = document.createElement('div');
 wrapper.classList.add('wrapper');
 bodyTag.appendChild(wrapper);
@@ -48,10 +54,7 @@ wrapperContent.append(title, textarea, keyboard, description, switchLanguage);
 function createKeys() {
   let str = '';
   for (let i = 0; i < keys.length; i++) {
-    if (keys[i] >= 65 && keys[i] <= 90) {
-      str += `<div class="keyboard__key k${keys[i]}">${String.fromCharCode(keys[i])}</div>`;
-    }
-    str += `<div class="keyboard__key k${keys[i]}">${String.fromCharCode(keys[i])} ${keys[i]} ${i}</div>`;
+    str += `<div class="keyboard__key k${keysCode[i]}" data='${keysCode[i]}'>${keys[i]}</div>`;
   }
   keyboard.innerHTML = str;
 }
